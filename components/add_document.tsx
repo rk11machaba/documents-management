0cd678689a2b4443e05c94350ce1b0503b5c6fa8
rk11machaba <machabarotshidzwakaizer@gmail.com>
2024-12-03 09:55:14 +0200

    update landing page

---
+++ components/add_document.tsx
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+
+export default function addDocument(){
+    const [ documentName, setDocumentName ] = useState('');
+    const [ description, setDescription ] = useState('');
+    const [ image, setImage ] = useState('');
+
+    
+}
